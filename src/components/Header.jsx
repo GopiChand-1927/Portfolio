35765f4b3457d47ac58a55bc8728ca4590fba6a5
gopichand.b <gopichand.b@cgg.gov.in>
2024-09-18 15:58:10 +0530

    ui changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import resume from '../assets/images/Gopi_Chand_Bhukya_Resume.pdf'
 const Header = () => {
   return (
    
-      <Navbar  expand="lg" className="sticky-top darkbg" >
+      <Navbar  expand="lg" className="sticky-top darkbg"  >
         <Container>
           <NavbarBrand className='fs-5 fw-bold text-white'>GOPI CHAND BHUKYA</NavbarBrand>
           <div className="ms-3 d-flex align-items-center">
@@ -18,7 +18,7 @@ const Header = () => {
               <NavLink href="#about" className="text-white hvr-underline-from-center">ABOUT</NavLink>
               <NavLink href="#skills" className="text-white hvr-underline-from-center">SKILLS</NavLink>
               <NavLink href="#projects" className="text-white hvr-underline-from-center">PROJECTS</NavLink>
-              <NavLink href={resume} target='_blank'><Button className='btn btn-success' style={{borderRadius:'20px',marginTop:'-5px'}}>Resume</Button></NavLink>
+              <NavLink href={resume} target='_blank'><Button className='btn btn-success' style={{borderRadius:'20px',marginTop:'-5px'}}>RESUME</Button></NavLink>
               
             </Nav>
           </NavbarCollapse>
